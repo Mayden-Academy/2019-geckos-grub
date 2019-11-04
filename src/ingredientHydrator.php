2f009446f95915206db791952e64013a9d061e9b
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-04 16:18:49 +0000

    changed typhint type to PDO

--- src/ingredientHydrator.php
+++ src/ingredientHydrator.php
@@ -15,7 +15,7 @@ class IngredientHydrator {
      *
      * @result populates results property with ingredients' names
      */
-    public function __construct(GRUB\Db $db)
+    public function __construct(\PDO $db)
     {
         $query = $db->prepare("SELECT `name` FROM `ingredients`;");
         $query->setFetchMode(PDO::FETCH_CLASS, 'IngredientEntity');
