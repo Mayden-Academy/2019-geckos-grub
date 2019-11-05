18bbc074664adbcc8b13e59197ffdd2c8a2ef205
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-05 13:21:26 +0000

    testing completed

---
+++ recipes.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Function to validate post data from form submitted from index.php
+ *
+ *
+ * @return array of validated ingredients
+ */
+function validateForm(array $formData) :array {
+    if ($formData != []){
+        $ingredients = [];
+        foreach ($formData as $ingredient => $state){
+            $ingredient = htmlentities($ingredient);
+            array_push($ingredients, $ingredient);
+        }
+        return $ingredients;
+    } else {
+       return 'Invalid data. Please try again.';
+    }
+}
