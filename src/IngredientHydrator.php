68ffebec75633d291b877a5f169aa48a2139a25b
jdselby24 <jdselby24@gmail.com>
2019-11-05 14:19:12 +0000

    Fixed filename case

---
+++ src/IngredientHydrator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GRUB;
+
+/**
+ * Class IngredientHydrator uses Db connection to fetch ingredients array from db
+ *
+ * @package GRUB
+ */
+class IngredientHydrator {
+    /**
+     * @var array of instantiated ingredient entity objects
+     */
+    public $ingredients;
+
+    /**
+     * IngredientHydrator constructor.
+     *
+     * @param Db $db
+     *
+     * @result populates results property with ingredients' names
+     */
+    public function __construct(\PDO $db)
+    {
+        $query = $db->prepare("SELECT `name` FROM `ingredients`;");
+        $query->setFetchMode(\PDO::FETCH_CLASS, 'GRUB\IngredientEntity');
+        $query->execute();
+        $this->ingredients = $query->fetchAll();
+    }
+
+    /**
+     * Instantiates ingredient entity objects
+     *
+     * @param array $results
+     *
+     * @return array of ingredient entity objects
+     */
+    public function getIngredients() {
+        return $this->ingredients;
+    }
+}
---
+++ src/RecipeHydrator.php
@@ -0,0 +1,32 @@
+<?php
+namespace GRUB;
+class RecipeHydrator
+{
+    /**
+     * @var array of recipe objects
+     */
+    private $recipes = [];
+    /**
+     * @var multidimensional array
+     */
+    private $recipesArray;
+
+    /**
+     * RecipeHydrator constructor.
+     * @param GRUB\Curl $curl takes a multidimensional array from Curl
+     */
+    public function __construct(Curl $curl){
+      $this->recipesArray = $curl->makeRequest()['results'];
+  }
+
+    /**
+     * @return array pass a array of objects(recipes) with all the information from the Curl
+     */
+    public function getRecipes():array {
+        foreach($this->recipesArray as $recipe){
+            $recipe = new RecipeEntity($recipe['title'], $recipe['href'], $recipe['thumbnail'], $recipe['ingredients']);
+            array_push($this->recipes, $recipe);
+        }
+        return $this->recipes;
+    }
+}
