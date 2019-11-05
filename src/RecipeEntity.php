0531453a5d636a9cd6e3183568f388f49e294e9d
jdselby24 <jdselby24@gmail.com>
2019-11-05 11:23:46 +0000

    made tests to code and added doc blocks.

--- src/RecipeEntity.php
+++ src/RecipeEntity.php
@@ -8,23 +8,45 @@ class RecipeEntity {
     private $imageURL = "";
     private $ingredients = "";
 
+    /**
+     * Instanciates RecipeEntity
+     *
+     * @param string $title Title of the recipe
+     * @param string $link Link to the recipe 
+     * @param string $imageURL URL of an image of the dish
+     * @param array $ingredients Array of the ingredients required for the recipe
+     */
     public function __construct(string $title, string $link, string $imageURL, array $ingredients){
               $this->title = $title;
               $this->link = $link;
               $this->imageURL = $imageURL;
               $this->ingredients = $ingredients;
     }
- 
-    public function getTitle(): string{
-        return $this->title;
-    }
-    public function getLink(): string{
-        return $this->link;
-    }
-    public function getImageURL(): string{
-        return $this->imageURL;
-    }
-    public function getIngredients(): array{
-        return $this->ingredients;
+
+    /**
+     * Generating HTML from recipe properties.
+     *
+     * @return string HTML code. 
+     */
+    public function generateHTML(): string {
+        $ingredients = "";
+
+        foreach($this->ingredients as $ingredient) {
+            $ingredients .= $ingredient .= " ";
+        }
+        
+        $html = "";
+        $html .= "<div class='recipe'>";
+        $html .= "<a href='$this->link'>";
+        $html .= "<h3>$this->title</h3>";
+        $html .= "<div class='recipeImage'>";
+        $html .= "<img src='$this->imageURL'/>";
+        $html .= "</div>";
+        $html .= "<p>Ingredients: $ingredients </p>";
+        $html .= "</a>";
+        $html .= "</div>";
+
+        return $html;
+
     }
 }
