d14807f038988e756612307de4786d182258beed
jdselby24 <jdselby24@gmail.com>
2019-11-06 15:51:09 +0000

    added doc blocks to recipe hydrator

--- src/Recipe/RecipeDBHydrator.php
+++ src/Recipe/RecipeDBHydrator.php
@@ -2,23 +2,38 @@
 
 namespace GRUB\Recipe;
 use PDO;
-
+/**
+ * Class to create recipe entities from DB
+ */
 class RecipeDBHydrator {
-
-    private $recipes;
+    /**
+     * Property to store DB object
+     *
+     * @var PDO 
+     */
     private $db;
 
+    /**
+     * Constructor that takes PDO object and stores in $db
+     *
+     * @param PDO $db
+     */
     public function __construct(PDO $db) 
     {
         $this->$db  = $db;    
     }
 
-    public function getRecipesFromDB() 
+    /**
+     * Function that retrives the recipe objects from the DB
+     *
+     * @return array Array of recipe objects
+     */
+    public function getRecipesFromDB() : array
     {
         $statement = "SELECT `title`,`link`,`imageURL`,`ingredients` FROM `recipes`;";
         $query = $this->db->prepare($statement);
         $query->setFetchMode(PDO::FETCH_CLASS, 'GRUB\Recipe\RecipeEntity');
         $query->execute();
+        return $query->fetchAll();
     }
-
 }
