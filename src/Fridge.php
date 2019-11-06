6610ba0821cabe0e3a3c44da226f6a6fcdcb8a16
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-06 15:23:11 +0000

    finished writing saveRecipe method

--- src/Fridge.php
+++ src/Fridge.php
@@ -18,11 +18,5 @@ class Fridge {
         $statement = "INSERT INTO `recipes` (`title`, `imageURL`, `link`, `ingredients`) VALUES (?, ?, ?, ?)";
         $query = $this->db->prepare($statement);
         return $query->execute([$recipe['title'], $recipe['imageURL'], $recipe['link'], $recipe['ingredients']]);
-
-
-        $query = $this->db->prepare("SELECT `name` FROM `ingredients`;");
-        $query->setFetchMode(\PDO::FETCH_CLASS, 'GRUB\IngredientEntity');
-        $query->execute();
-        return $query->fetchAll();
     }
 }
