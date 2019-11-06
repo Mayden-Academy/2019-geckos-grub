247d153b1b4a93f35b57fa8d7b97e8aa39dccc28
bentleybiscuit <charlottenightingale@ymail.com>
2019-11-06 11:50:25 +0000

    review changes including extra space at base of page

--- index.php
+++ index.php
@@ -30,9 +30,9 @@ if(isset($_GET['message'])) {
             </div>
             <form method="post" action="recipes.php">
                 <?php echo $ingredientForm; ?>
-                <h6>Extra ingredients (single words separated by spaces): </h6>
-                <input class= "extras" type="text" name="userIngredients"/>
-                <input class = "submit" type="submit">
+                <h6>Extra ingredients (separated by spaces): </h6>
+                <input class="extras" type="text" name="userIngredients" placeholder="marmite apple spinach beetroot"/>
+                <input class="submit" type="submit">
             </form>
         </div>
     </body>
