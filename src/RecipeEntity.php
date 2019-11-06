88b449122fad26ad32c5b992a25a4ff593b4064b
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-06 15:36:21 +0000

    changed namespacing on RecipeEntity.php

--- src/RecipeEntity.php
+++ src/RecipeEntity.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GRUB;
+namespace GRUB\recipe;
 
 /**
  * Class for a recipe object
--- tests/src/RecipeEntity.php
+++ tests/src/RecipeEntity.php
@@ -62,4 +62,62 @@ class StackTest extends TestCase {
         $this->expectException(TypeError::class);
         $case = new GRUB\RecipeEntity ([],[],[],[]);
     }
+
+    /**
+     * Testing for success with generating HTML
+     *
+     * @return void
+     */GenerateHTML
+    public function testSuccessRecipeEntity(){
+        $testIngredient = new GRUB\RecipeEntity ('string','string','string','string');
+        $result = $testIngredient->generateHTML();
+        $expected = "<div class='recipe'>";
+        $expected .= "<div class='left'>";
+        $expected .= "<div class='recipeImage'>";
+        $expected .= "<img src='string'/>";
+        $expected .= "</div>";
+        $expected .= "<a href='string'>";
+        $expected .= "<button>Link to recipe</button>";
+        $expected .= "</a>";
+        $expected .= "</div>";
+        $expected .= "<div class='right'>";
+        $expected .= "<h5>string</h5>";
+        $expected .= "<p>Ingredients: string </p>";
+        $expected .= "</div>";
+        $expected .= "</div>";
+        $this->assertEquals($expected, $result);
+    }
+    /**
+     * Testing for failure in generating HTML. Passed inergers as opposed to strings.
+     *
+     * @return void
+     */
+    public function testFailureRecipeEntityGenerateHTML(){
+        $testIngredient = new GRUB\RecipeEntity (9,9,9,9);
+        $result = $testIngredient->generateHTML();
+        $expected = "<div class='recipe'>";
+        $expected .= "<div class='left'>";
+        $expected .= "<div class='recipeImage'>";
+        $expected .= "<img src='9'/>";
+        $expected .= "</div>";
+        $expected .= "<a href='9'>";
+        $expected .= "<button>Link to recipe</button>";
+        $expected .= "</a>";
+        $expected .= "</div>";
+        $expected .= "<div class='right'>";
+        $expected .= "<h5>9</h5>";
+        $expected .= "<p>Ingredients: 9 </p>";
+        $expected .= "</div>";
+        $expected .= "</div>";
+        $this->assertEquals($expected, $result);
+    }
+    /**
+     * Testing for malformed code. Passed in arrays not strings.
+     *
+     * @return void
+     */
+    public function testMalformedRecipeEntityGenerateHTML(){
+        $this->expectException(TypeError::class);
+        $case = new GRUB\RecipeEntity ([],[],[],[]);
+    }
 }
