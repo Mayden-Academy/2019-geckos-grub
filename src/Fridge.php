317b1b4ff7e06897d500464dc3c6c54bd719752e
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-06 15:13:47 +0000

    populated saveRecipe method

--- src/Fridge.php
+++ src/Fridge.php
@@ -2,6 +2,8 @@
 
 namespace GRUB;
 
+use PDO;
+
 class Fridge {
 
     public $db;
@@ -13,6 +15,16 @@ class Fridge {
 
     public function saveRecipe(array $recipe): array
     {
+        function addNewSet(array $newSet, PDO $db) {
 
+        $statement = "INSERT INTO `recipes` (`name`, `cards`, `released`) VALUES (?, ?, ?)";
+        $query = $db->prepare($statement);
+        return $query->execute([$newSet['name'], $newSet['cards'], $newSet['released']]);
+
+
+        $query = $this->db->prepare("SELECT `name` FROM `ingredients`;");
+        $query->setFetchMode(\PDO::FETCH_CLASS, 'GRUB\IngredientEntity');
+        $query->execute();
+        return $query->fetchAll();
     }
 }
