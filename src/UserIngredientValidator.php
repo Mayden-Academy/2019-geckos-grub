47c40e91b413a9c15567bf4f8519fb6696fdb61a
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-06 10:08:17 +0000

    Fixed the self/this problem by moving  into the function. Added second required parameter to array_push().

--- src/UserIngredientValidator.php
+++ src/UserIngredientValidator.php
@@ -5,9 +5,7 @@ namespace GRUB;
 * Abstract class with a static function used for validation of user-inputted ingredients
 */
 abstract class UserIngredientValidator {
-	private $validatedIngredients = [];
-	
-	/**
+    /**
 	* Function to validate ingredients input by user and return a 'clean' array of ingredients with whitespace removed
 	*
 	* @param array $explodedUserIngredients an array of the user ingredients submitted from index.php
@@ -15,14 +13,15 @@ abstract class UserIngredientValidator {
 	* @returns array of validated user-inputted ingredients
 	*/
 	public static function validateUserIngredients(array $explodedUserIngredients) :array {
+        private $validatedIngredients = [];
 		if ($explodedUserIngredients != []){
 			foreach ($explodedUserIngredients as $ingredient){
 				//removes anything that isn't a letter and removes all whitespace
 				//the letter i at the end is used to remove case sensitivity from the regex
 				$validatedIngredient = preg_replace( '/[^a-z]/i', '', $ingredient);
-				array_push(self::validatedIngredients);
+				array_push($validatedIngredients, $validatedIngredient);
 			}
-			return self::$validatedIngredients;
+			return $validatedIngredients;
 		} else {
 			return "Invalid data. Please try again.";
 		}
