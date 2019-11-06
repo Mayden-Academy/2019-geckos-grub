33ddeba84ac6910b76d6e7c76b0c54130a34dbe1
jdselby24 <jdselby24@gmail.com>
2019-11-06 10:00:45 +0000

    Added calums user ingredient validator

---
+++ src/UserIngredientValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GRUB;
+/*
+* Abstract class with a static function used for validation of user-inputted ingredients
+*/
+abstract class UserIngredientValidator {
+	private $validatedIngredients = [];
+	
+	/**
+	* Function to validate ingredients input by user and return a 'clean' array of ingredients with whitespace removed
+	*
+	* @param array $explodedUserIngredients an array of the user ingredients submitted from index.php
+	*
+	* @returns array of validated user-inputted ingredients
+	*/
+	public static function validateUserIngredients(array $explodedUserIngredients) :array {
+		if ($explodedUserIngredients != []){
+			foreach ($explodedUserIngredients as $ingredient){
+				//removes anything that isn't a letter and removes all whitespace
+				//the letter i at the end is used to remove case sensitivity from the regex
+				$validatedIngredient = preg_replace( '/[^a-z]/i', '', $ingredient);
+				array_push(self::validatedIngredients);
+			}
+			return self::$validatedIngredients;
+		} else {
+			return "Invalid data. Please try again.";
+		}
+	}
+}
