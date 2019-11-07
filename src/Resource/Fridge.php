42076de5f7ed9190738e0221e4c77b9ce8b23aa0
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-07 10:05:04 +0000

    corrected saveRecipe with bindparams

--- src/Resource/Fridge.php
+++ src/Resource/Fridge.php
@@ -25,9 +25,13 @@ class Fridge {
      */
     public function saveRecipe(array $recipe): bool
     {
-        $statement = "INSERT INTO `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (?, ?, ?, ?)";
+        $statement = "INSERT INTO `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (:title, :link, :imageURL, :ingredients)";
         $query = $this->db->prepare($statement);
-        return $query->execute([$recipe['title'], $recipe['link'], $recipe['imageURL'], $recipe['ingredients']]);
+        $query->bindParam(":title", $recipe['title']);
+        $query->bindParam(":link", $recipe['link']);
+        $query->bindParam(":imageURL", $recipe['imageURL']);
+        $query->bindParam(":ingredients", $recipe['ingredients']);
+        return $query->execute();
     }
     /**
      * @param array $recipe the recipe to be delete by the user
