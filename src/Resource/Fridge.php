d89db598bec8c711b53c6f5aaaa9ea44ebaf5b6b
jdselby24 <jdselby24@gmail.com>
2019-11-07 10:30:14 +0000

    :

--- src/Resource/Fridge.php
+++ src/Resource/Fridge.php
@@ -25,9 +25,13 @@ class Fridge {
      */
     public function saveRecipe(array $recipe): bool
     {
-        $statement = "INSERT INTO `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (?, ?, ?, ?)";
+        $statement = "INSERT INTO `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (:title, :link, :imageURL, :ingredients)";
         $query = $this->db->prepare($statement);
-        return $query->execute([$recipe['title'], $recipe['link'], $recipe['imageURL'], $recipe['ingredients']]);
+        $query->bindParam(":title", $recipe['title']);
+        $query->bindParam(":link", $recipe['link']);
+        $query->bindParam(":imageURL", $recipe['imageURL']);
+        $query->bindParam(":ingredients", $recipe['ingredients']);
+        return $query->execute();
     }
     /**
      * @param array $recipe the recipe to be delete by the user
@@ -36,8 +40,12 @@ class Fridge {
      */
     public function deleteRecipe(array $recipe): bool
     {
-        $statement = "DELETE FROM `recipes` (`title`, `link`, `imageURL`, `ingredients`) WHERE (?, ?, ?, ?)";
+        $statement = "DELETE FROM `recipes` (`title`, `link`, `imageURL`, `ingredients`) WHERE (:title, :link, :imageURL, :ingredients)";
         $query = $this->db->prepare($statement);
-        return $query->execute([$recipe['title'], $recipe['link'], $recipe['imageURL'], $recipe['ingredients']]);
+        $query->bindParam(":title", $recipe['title']);
+        $query->bindParam(":link", $recipe['link']);
+        $query->bindParam(":imageURL", $recipe['imageURL']);
+        $query->bindParam(":ingredients", $recipe['ingredients']);
+        return $query->execute();
     }
 }
