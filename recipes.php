b2d24a73468c4483e8e67fb7b8c00b3d48290d6e
jdselby24 <jdselby24@gmail.com>
2019-11-07 12:38:08 +0000

    added doc block/comments

--- recipes.php
+++ recipes.php
@@ -24,7 +24,9 @@ if($_POST != []) {
         $htmlOut =  "<h1>No recipes found, please select different ingredients</h1>";
     }
 } else {
-    // Grabs session ingredients 
+    /* Grabs session ingredients if present
+    * so that
+    */
     if(isset($_SESSION['ingredients'])) {
         $htmlOut = '';
         $formDataHandler = new GRUB\Resource\FormDataHandler();
