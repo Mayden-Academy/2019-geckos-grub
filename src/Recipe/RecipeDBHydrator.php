acc88ed69bdb770c7310e08918be0bc6b31a1384
jdselby24 <jdselby24@gmail.com>
2019-11-07 14:45:08 +0000

    FETCH_CLASS fix

--- src/Recipe/RecipeDBHydrator.php
+++ src/Recipe/RecipeDBHydrator.php
@@ -36,7 +36,7 @@ class RecipeDBHydrator
     {
         $statement = "SELECT `title`, `link`, `imageURL`, `ingredients` FROM `recipes`;";
         $query = $this->db->prepare($statement);
-        $query->setFetchMode(PDO::FETCH_CLASS, "GRUB\Recipe\RecipeEntity");
+        $query->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "GRUB\Recipe\RecipeEntity");
         $query->execute();
         $recipesOut =  $query->fetchAll();
 
