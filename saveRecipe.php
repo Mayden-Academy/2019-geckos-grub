0cd28f846636599ad091fa0c15df1f04a8291068
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-07 11:16:24 +0000

    improved whitespace

--- saveRecipe.php
+++ saveRecipe.php
@@ -6,7 +6,7 @@ use GRUB\Resource\Fridge;
 use GRUB\Resource\Db;
 
 if($_POST !=[]){
-    $thefridge =new Fridge(Db::getDB());
+    $thefridge = new Fridge(Db::getDB());
     $thefridge->saveRecipe($_POST);
     header("Location: savedRecipes.php");
 }
