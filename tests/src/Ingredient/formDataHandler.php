eb600270791677ef5c0a3bc4d5b10985da750061
Josh Selby <jdselby24@gmail.com>
2019-11-07 16:43:03 +0000

    Fixed version of FDH test

---
+++ tests/src/Ingredient/formDataHandler.php
@@ -0,0 +1,38 @@
+<?php
+require_once('../../../src/Ingredient/FormDataHandler.php');
+require_once('../../../src/Validator/Validator.php');
+require_once('../../../src/Validator/UserIngredientValidator.php');
+use PHPUnit\Framework\TestCase;
+class FormTest extends TestCase
+{
+    /**
+     * Tests for success of the FormDataHandler function
+     */
+    public function testFormDataHandlerSuccess()
+    {
+        $formData = ["onions" => "on", "turnip" => "on", "lemon" => "on", 'userIngredients' => 'mint lamb'];
+        $formDataHandler = new GRUB\Resource\FormDataHandler();
+        $validation = $formDataHandler->processData($formData);
+        $expected = ["onions", "turnip", "lemon", 'mint', 'lamb'];
+        $this->assertEquals($expected, $validation);
+    }
+    /**
+     * Tests for graceful failure of the FormDataHandler function
+     */
+    public function testFormDataHandlerFailure()
+    {
+        $formData = [10, 41, 3259703498, '345'];
+        $formDataHandler = new GRUB\Resource\FormDataHandler();
+        $validation = $formDataHandler->processData($formData);
+        $expected = ['', '1', '2', '3'];
+        $this->assertEquals($validation, $expected);
+    }
+//  Tests the type-hinting of the FormDataHandler function
+    public function testFormDataHandlerMalformed()
+    {
+        $formData = "beans";
+        $this->expectException(TypeError::class);
+        $formDataHandler = new GRUB\Resource\FormDataHandler();
+        $case = $formDataHandler->processData($formData);
+    }
+}
