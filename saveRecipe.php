1eab2e2695b8cacf7ff22d29f6041b5316222c1b
bentleybiscuit <charlottenightingale@ymail.com>
2019-11-07 11:12:47 +0000

    created saveRecipe.php

---
+++ saveRecipe.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use GRUB\Resource\Fridge;
+use GRUB\Resource\Db;
+
+if($_POST !=[]){
+    $thefridge =new Fridge(Db::getDB());
+    $thefridge->saveRecipe($_POST);
+    header("Location: savedRecipes.php");
+}
