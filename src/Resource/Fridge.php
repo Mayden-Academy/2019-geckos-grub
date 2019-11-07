0ea9d36f9ac57a4b06c008318a358904a112ed9d
jdselby24 <jdselby24@gmail.com>
2019-11-07 13:37:47 +0000

    added duplicate save prevention

--- src/Resource/Fridge.php
+++ src/Resource/Fridge.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace GRUB\Resource;
+use PDO;
 
 class Fridge {
 
     /**
-     * @var \PDO database connection
+     * @var PDO database connection
      */
     private $db;
 
@@ -13,7 +14,7 @@ class Fridge {
      * Fridge constructor.
      * @param \PDO $db database connection
      */
-    public function __construct(\PDO $db)
+    public function __construct(PDO $db)
     {
         $this->db = $db;
     }
@@ -25,13 +26,34 @@ class Fridge {
      */
     public function saveRecipe(array $recipe): bool
     {
-        $statement = "INSERT INTO `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (:title, :link, :imageURL, :ingredients)";
+        // Query based on submitted URL
+
+        $statement = "SELECT `link` FROM `recipes` WHERE `link` = :link ";
         $query = $this->db->prepare($statement);
-        $query->bindParam(":title", $recipe['title']);
+        $query->setFetchMode(PDO::FETCH_ASSOC);
         $query->bindParam(":link", $recipe['link']);
-        $query->bindParam(":imageURL", $recipe['imageURL']);
-        $query->bindParam(":ingredients", $recipe['ingredients']);
-        return $query->execute();
+        $query->execute();
+        $existingRecipes = $query->fetchAll();
+
+        // 'If' checking if there is an existing recipe with that URL
+
+        if(count($existingRecipes) > 0) {
+            $statement = "UPDATE `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (:title, :link, :imageURL, :ingredients) WHERE `link` = :link;";
+            $query = $this->db->prepare($statement);
+            $query->bindParam(":title", $recipe['title']);
+            $query->bindParam(":link", $recipe['link']);
+            $query->bindParam(":imageURL", $recipe['imageURL']);
+            $query->bindParam(":ingredients", $recipe['ingredients']);
+            return $query->execute();
+        } else {
+            $statement = "INSERT INTO `recipes` (`title`, `link`, `imageURL`, `ingredients`) VALUES (:title, :link, :imageURL, :ingredients)";
+            $query = $this->db->prepare($statement);
+            $query->bindParam(":title", $recipe['title']);
+            $query->bindParam(":link", $recipe['link']);
+            $query->bindParam(":imageURL", $recipe['imageURL']);
+            $query->bindParam(":ingredients", $recipe['ingredients']);
+            return $query->execute();
+        }
     }
     /**
      * @param array $recipe the recipe to be delete by the user
